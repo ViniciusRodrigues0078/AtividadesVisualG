00c7c7541b7cb242b8e7286f77800dce8c787cb3
ViniciusRodrigues0078 <vinicius.rodrigues.g.m.pereira@gmail.com>
2024-10-30 14:49:54 -0300

    .

---
+++ Atv17.py
@@ -0,0 +1,5 @@
+"""Escreva um programa que leia um vetor de 15 posições de inteiros. Em seguida, o programa
+deve ler um valor inteiro e imprimir o número de vezes que este valor ocorre no vetor
+https://www.w3schools.com/python/python_lists.asp
+https://www.w3schools.com/python/python_for_loops.asp
+"""
